ffd61f23217e283674dac9d5cd88073e3229d1ec
beescuit9510 <pengsoon20@gmail.com>
2022-12-11 15:45:34 +0900

    feat: implement Event Sync by Storing events

--- query/index.js
+++ query/index.js
@@ -49,6 +49,13 @@ app.post('/events', (req, res) => {
   return res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Listening on 4002');
+
+  const res = await axios.get('http://localhost:4005/events');
+
+  for (let event of res.data) {
+    console.log('Processing event:', event.type);
+    handleEvent(event.type, event.data);
+  }
 });
