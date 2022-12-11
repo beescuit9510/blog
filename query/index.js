773b68adf8fd3344d62cc928e32a7de51d0d5b53
beescuit9510 <pengsoon20@gmail.com>
2022-12-11 15:02:07 +0900

    feat: implement option 3 (QueryService only listens for 'simple update' event approach) for new feature (CommentModeration)

--- query/index.js
+++ query/index.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
+
+const handleEvent = (type, data) => {
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+};
 
 app.get('/posts', (req, res) => {
   return res.send(posts);
@@ -15,19 +44,7 @@ app.get('/posts', (req, res) => {
 app.post('/events', (req, res) => {
   const { type, data } = req.body;
 
-  if (type === 'PostCreated') {
-    const { id, title } = data;
-
-    posts[id] = { id, title, comments: [] };
-  }
-
-  if (type === 'CommentCreated') {
-    const { id, content, postId } = data;
-
-    const post = posts[postId];
-
-    post.comments.push({ id, content });
-  }
+  handleEvent(type, data);
 
   return res.send({});
 });
