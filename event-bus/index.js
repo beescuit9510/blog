bf93613ba550abc15ed9cb951f3c99f4b6b25e88
beescuit9510 <pengsoon20@gmail.com>
2022-12-11 12:43:16 +0900

    fix: parsing body bug in evnet bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', (req, res) => {
-  const { event } = req.body;
+  const event = req.body;
 
   axios.post('https://localhost:4000/events', event);
   axios.post('https://localhost:4001/events', event);
